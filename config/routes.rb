c7e3b9cc75f0adc547ffbabdb8adb6399e6ae134
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-12-03 04:07:45 +0900

    devise導入 Userモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+  }
 end
--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Home", type: :request do
-
   describe "GET /top" do
     it "Top画面が表示されること" do
       get root_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
