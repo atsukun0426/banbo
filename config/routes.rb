5521d86dd1094d12ebae62f380d60a7bc67aaeeb
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2021-01-17 14:03:44 +0900

    organizer/registrations/new　bootstrap実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   devise_for :organizers, controllers: {
     sessions: 'organizers/sessions',
     passwords: 'organizers/passwords',
-    registrations: 'organizers/registrations'
+    registrations: 'organizers/registrations',
   }
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
   }
   resources :users, only: [:index, :show]
   get 'chat/:id' => 'chats#show', as: 'chat'
--- spec/factories/organizers.rb
+++ spec/factories/organizers.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :organizer do
-    
   end
 end
